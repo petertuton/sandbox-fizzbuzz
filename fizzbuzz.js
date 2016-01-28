090f68e9dc7a928c39cd3582a73214e3adfea9c6
ptuton <ptuton@gmail.com>
2016-01-29 09:55:21 +1000

    implement fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,4 +5,17 @@ FizzBuzz.prototype.divisibleBy = function(number, divisor) {
   return number % divisor === 0;
 };
 
+FizzBuzz.prototype.convertToFizzBuzz = function(number) {
+  if (this.divisibleBy(number, 3)) {
+    return "Fizz";
+  }
+  if (this.divisibleBy(number, 5)) {
+    return "Buzz";
+  }
+  if (this.divisibleBy(number, 15)) {
+    return "FizzBuzz";
+  }
+  return number.toString();
+};
+
 module.exports = FizzBuzz;
